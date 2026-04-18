7359f2c7c553432c66e8d895bf815451b485e30e
grigorij018 <grigorij018@mail.ru>
2026-04-18 22:18:57 +0300

    fix: resolve checkstyle issues in main service

--- ewm-main-service/src/main/java/ru/practicum/ewm/main/event/EventService.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/main/event/EventService.java
@@ -29,10 +29,8 @@ import ru.practicum.ewm.main.user.UserService;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
