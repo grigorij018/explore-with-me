701aa378a77e8446b856e3a8bdf7636cfa3f331a
grigorij018 <grigorij018@mail.ru>
2026-05-11 15:16:47 +0300

    add home v4

--- ewm-main-service/src/main/java/ru/practicum/ewm/main/HomeController.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/main/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Explore With Me is running on localhost:8080";
+        return "Explore With Me is running on localhost:8080 V2";
     }
 }
